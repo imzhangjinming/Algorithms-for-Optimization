9e03030d88688a20c192a0690898180eb41b40a9
Jinming Zhang <jinmingzhang@192.168.0.101>
2022-07-27 10:05:10 +0800

    first commit

---
+++ ch3_bracketing/bracketing.h
@@ -0,0 +1,103 @@
+#ifndef __BRACKETING_H__
+#define __BRACKETING_H__
+
+#include<math.h>
+
+template<typename T>
+struct Interval
+{
+    T lowerBound;
+    T upperBound;
+};
+
+// algorithm 3.1
+// brief: 计算单峰函数极小值点所在的区间
+// param: 
+//      f: 指向单峰函数的指针
+//      x: 初始点
+//      step: 寻找极小值点时采用的步长
+//      k: 步长增长的倍数
+// birth: created by ZJM on 20220726
+template<typename T>
+Interval<T> bracketMinimum(T (*f)(T), T x, T step, T k){
+    T a = x;
+    T ya = (*f)(a);
+    T b = x + step;
+    T yb = (*f)(b);
+    if (yb > ya){
+        step = -step;
+        b = x + step;
+        yb = (*f)(b);
+    }
+    T c, yc;
+    while(1){
+        c = b + step;
+        yc = (*f)(c);
+        if(yc > yb){
+            Interval<T> interval;
+            if(a < c){          
+                interval.lowerBound = a;
+                interval.upperBound = c;
+                return interval;
+            }else{
+                interval.lowerBound = c;
+                interval.upperBound = a;
+                return interval;
+            }
+        }
+        a = b;
+        ya = yb;
+        b = c;
+        yb = yc;
+        step *= k;
+    }
+}
+
+// algorithm 3.2
+// brief: fibonacci search
+// param: 
+//      f: 指向单峰函数的指针
+//      a,b: 包含函数极小值点的区间
+//      n: 调用单峰函数的次数
+//      epsilon: 
+// birth: created by ZJM on 20220727
+template<typename T>
+Interval<T> fibonacciSearch(T (*f)(T), T a, T b, int n, T epsilon){
+    T s = (1 - sqrt(5)) / (1 + sqrt(5));
+    T phi = (1 + sqrt(5)) / 2;
+    T rho = 1 / (phi * (1 - pow(s, n+1)) / (1 - pow(s, n)));
+    T d = rho * b + (1 - rho) * a;
+    T yd = (*f)(d);
+    T c, yc;
+    for(int i = 0; i < n-1; i++){
+        if(i == n-2){
+            c = epsilon * a + (1 - epsilon) * d;
+        }
+        else{
+            c = rho * a + (1 - rho) * b;
+        }
+        yc = (*f)(c);
+        if(yc < yd){
+            b = d;
+            d = c;
+            yd = yc;
+        }
+        else{
+            a = b;
+            b = c;
+        }
+        rho = 1 / (phi * (1 - pow(s, n-i)) / (1 - pow(s, n-i-1)));
+    }
+    Interval<T> interval;
+    if(a < b){
+        interval.lowerBound = a;
+        interval.upperBound = b;
+    }
+    else{
+        interval.lowerBound = b;
+        interval.upperBound = a;
+    }
+    return interval;
+}
+
+#endif
---
+++ ch3_bracketing/main.cpp
@@ -0,0 +1,39 @@
+#include"bracketing.h"
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+// Unimodality function
+template<typename T>
+T unimodalityFunc(T x){
+    return x * x - 2 * x + 1;
+}
+
+int main(void){
+    // test algorithm 3.1
+    cout << "algorithm 3.1" << endl;
+    Interval<double> interval1 = bracketMinimum<double>(unimodalityFunc<double>, 0.0, 1e-2, 2.0);
+    cout << "lower bound: " << interval1.lowerBound << endl;
+    cout << "upper bound: " << interval1.upperBound << endl;
+
+    Interval<float> interval2 = bracketMinimum<float>(unimodalityFunc<float>, 0.0, 1e-2, 2.0);
+    cout << "lower bound: " << interval1.lowerBound << endl;
+    cout << "upper bound: " << interval1.upperBound << endl;
+
+    Interval<int> interval3 = bracketMinimum<int>(unimodalityFunc<int>, 0, 1, 2);
+    cout << "lower bound: " << interval3.lowerBound << endl;
+    cout << "upper bound: " << interval3.upperBound << endl;
+    
+    // test algorithm 3.2
+    cout << "algorithm 3.2" << endl;
+    Interval<double> interval4 = fibonacciSearch<double>(unimodalityFunc<double>, interval1.lowerBound, interval1.upperBound, 8, 0.01l);
+    cout << "lower bound: " << interval4.lowerBound << endl;
+    cout << "upper bound: " << interval4.upperBound << endl;
+
+    Interval<float> interval5 = fibonacciSearch<float>(unimodalityFunc<float>, interval2.lowerBound, interval2.upperBound, 8, 0.01f);
+    cout << "lower bound: " << interval5.lowerBound << endl;
+    cout << "upper bound: " << interval5.upperBound << endl;       
+
+    return 0;
+}
